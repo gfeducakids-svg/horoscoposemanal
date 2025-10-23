dcdf09867e3b5ae3668b5c6eabfcd0e9093430e0
Educa KIDS <gf.educakids@gmail.com>
2025-10-23 02:27:58 +0000

    Try fixing this error: `Build Error: Parsing css source code failed. Err

--- src/components/quiz/StarryBackground.tsx
+++ src/components/quiz/StarryBackground.tsx
@@ -1,6 +1,6 @@
 export function StarryBackground() {
   return (
-    <div className="fixed top-0 left-0 w-full h-full -z-10 bg-gradient-to-b from-[#0D0221] to-[#1A0B2E]">
+    <div className="fixed top-0 left-0 w-full h-full -z-10 bg-gradient-to-b from-[#0D0221] to-[#1A0B2E] overflow-hidden">
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
